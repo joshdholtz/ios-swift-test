8b4feca7e7e3ed3b3c542122bf30bb98112413c4
Josh Holtz <josh@rokkincat.com>
2014-06-02 22:05:52 -0500

    Initial project

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Josh Holtz on 6/2/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
